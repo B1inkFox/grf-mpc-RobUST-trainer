4e42ee9bfbd330b001b765d62b0fed28dc23596f
Darren Biskup <db3728@columbia.edu>
2025-12-27 02:08:57 -0500

    change things back to struct in datastructures

--- Darren RobUST Controller/Assets/Scripts/DataStructures.cs
+++ Darren RobUST Controller/Assets/Scripts/DataStructures.cs
@@ -3,10 +3,9 @@ using UnityEngine;
 /// <summary>
 /// A simple data structure to hold the position and rotation of a tracker.
 /// NOTE: Data is stored in a RIGHT-HANDED coordinate system (OpenVR standard).
-/// This is a non-MonoBehaviour class, used for organizing data efficiently.
 /// </summary>
 [System.Serializable]
-public class TrackerData
+public struct TrackerData
 {
     /// <summary>
     /// The full 4x4 homogeneous transformation matrix representing the tracker's pose.
@@ -22,10 +21,9 @@ public class TrackerData
 
 /// <summary>
 /// A simple data structure to hold force and moment data from a force plate.
-/// This is a non-MonoBehaviour class, used for organizing data.
 /// </summary>
 [System.Serializable]
-public class ForcePlateData
+public struct ForcePlateData
 {
     public Vector3 Force;
     public Vector3 CenterOfPressure;
